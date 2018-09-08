92e13acf82e78571617674da23fabe9edc341b3f
Malcolm Leake <echelon1089@gmail.com>
2018-09-08 23:35:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,4 @@
+
 #write your code here
 
 def countdown(number)
@@ -20,3 +21,27 @@ def countdown_with_sleep(number)
   end
   return "HAPPY NEW YEAR!"
   end
+  
+  def investor(deal)
+deal = 1
+while deal < 10
+  puts "I finally made #{deal} deals."
+  deal += 1
+  sleep(1)
+ end
+  puts "I'm finally getting a hang of this"
+  sleep(2)
+puts "A real programmer in the making."
+puts "Okay well enough about me and my deals, let me know a little more about you :)"
+sleep(2)
+puts "So stranger, Whats your name?"
+name = gets.chomp
+if name == "Malcolm" 
+  puts "Ohhh the wise one, the high up programming Gods told me this day would come, Welcome!!"
+  elsif name == "Thu"
+  puts "hmm, I heard you was in NYU for Dental, am I right?"
+else
+  puts "Nice to meet you #{name}, its great to have a friend to actually talk too."
+  end
+  puts "So what do you like to do one your free time?"
+end
